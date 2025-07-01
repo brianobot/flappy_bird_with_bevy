503aa79c19f3cf9ce2ab09a0cee60b1e5c78a728
Brian Obot <brianobot@gmail.com>
2025-07-01 18:26:46 +0100

    Update README with transform component details and remove unused asset files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::ui::{BorderColor, UiRect};
+use bevy::{prelude::*, transform, window::PrimaryWindow};
+
 use rand::{Rng, rng, rngs::ThreadRng};
 // use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 
@@ -38,10 +40,10 @@ fn main() {
         // .add_plugins(WorldInspectorPlugin::new())
         .add_systems(Startup, setup_level)
         .add_systems(Update, update_bird)
-        // .add_systems(Update, update_obstacles)
+        .add_systems(Update, update_obstacles)
+        .add_systems(Update, debug_cursor_position)
         .run();
 }
-
 
 #[derive(Resource)]
 struct GameManager {
@@ -50,9 +52,6 @@ struct GameManager {
     pub pipe_image: Handle<Image>,
     pub window_dimensions: Vec2,
 }
-
-#[derive(Component)]
-struct Special {}
 
 #[derive(Component)]
 struct Bird {
@@ -64,6 +63,8 @@ struct Obstacle {
     pipe_direction: f32,
 }
 
+#[derive(Component)]
+struct CoordinateText;
 
 fn setup_level(
     mut commands: Commands,
@@ -78,15 +79,16 @@ fn setup_level(
     // this Window component has some attributes about the window in Question
     // these attributes are important for the structure of the game as we would see later on
     let window = window_query.single_mut().unwrap();
- 
+
     commands.insert_resource(GameManager {
         pipe_image: pipe_image.clone(),
         window_dimensions: Vec2::new(window.width(), window.height()),
     });
-    
+
     // adding a resource more than once simply replaces the former instance with the new instance
     commands.insert_resource(GameManager {
         pipe_image: pipe_image.clone(),
+        // this is helpful to store so we don't keep query for the window in other systems
         window_dimensions: Vec2::new(window.width(), window.height()),
     });
 
@@ -102,8 +104,24 @@ fn setup_level(
     // Cameras in Bevy are mandatory to see anything: they configure the rendering.
 
     commands.spawn((
-        Sprite::from_image(bird_image),
-        Transform::IDENTITY.with_scale(Vec3::splat(PIXEL_RATIO)),
+        // Sprite::from_image(bird_image.clone()),
+        // A Transform is what allows you to place an object in the game world. It is a combination of the object's
+        // "translation" (position/coordinates), "rotation", and "scale" (size adjustment).
+        Transform::IDENTITY.with_scale(Vec3::splat(PIXEL_RATIO)), // makes the image PIXEL_RATIO times larger in all dimensions
+        // Transform has 3 fields
+        // - translation: moves objects around
+        // - rotation: rotate objects
+        // - scale: make them larger or smaller
+        Bird { velocity: 0. },
+    ));
+
+    commands.spawn((
+        Sprite::from_image(bird_image.clone()),
+        Transform {
+            translation: Vec3::splat(0.),
+            scale: Vec3::splat(4.),
+            ..Default::default()
+        },
         Bird { velocity: 0. },
     ));
 
@@ -111,13 +129,15 @@ fn setup_level(
     spawn_obstacles(&mut commands, &mut rand, window.width(), &pipe_image);
 }
 
-
 fn update_bird(
-    mut bird_query: Query<(&mut Bird, &mut Transform)>,
     time: Res<Time>,
+    mut commands: Commands,
+    mut bird_query: Query<(&mut Bird, &mut Transform), Without<Obstacle>>,
+    mut obstacle_query: Query<(Entity, &mut Transform), With<Obstacle>>,
     keys: Res<ButtonInput<KeyCode>>,
+    game_manager: Res<GameManager>,
 ) {
-    if let Ok((mut bird, mut transform)) = bird_query.single_mut() {
+    for (mut bird, mut transform) in bird_query.iter_mut() {
         if keys.just_pressed(KeyCode::Space) {
             bird.velocity = FLAP_FORCE;
         }
@@ -129,9 +149,40 @@ fn update_bird(
             Vec3::Z,
             f32::clamp(bird.velocity as f32 / VELOCITY_TO_ROTATION, -90., 90.).to_radians(),
         );
+
+        let mut dead = false;
+        if transform.translation.y <= -game_manager.window_dimensions.y / 2. {
+            dead = true
+        } else {
+            for (_entity, obstacle_transform) in obstacle_query.iter() {
+                if (obstacle_transform.translation.y - transform.translation.y).abs()
+                    < OBSTACLE_HEIGHT * PIXEL_RATIO / 2.
+                    && (obstacle_transform.translation.x - transform.translation.x).abs()
+                        < OBSTACLE_WIDTH * PIXEL_RATIO / 2.
+                {
+                    dead = true
+                }
+            }
+        }
+
+        if dead {
+            transform.translation = Vec3::ZERO;
+            bird.velocity = 0.;
+
+            for (entity, _obstacle_transform) in obstacle_query.iter_mut() {
+                commands.entity(entity).despawn();
+            }
+
+            let mut rand = rng();
+            spawn_obstacles(
+                &mut commands,
+                &mut rand,
+                game_manager.window_dimensions.x,
+                &game_manager.pipe_image.clone(),
+            );
+        }
     }
 }
-
 
 fn spawn_obstacles(
     commands: &mut Commands,
@@ -142,9 +193,9 @@ fn spawn_obstacles(
     for i in 0..OBSTACLE_AMOUNT {
         let y_offset = generate_offset(rand);
         let x_pos = window_width / 2.0 * OBSTACLE_SPACING * PIXEL_RATIO * i as f32;
-        
+
         info!("index: {} [Y Offset: {}, X Pos: {}]", i, y_offset, x_pos);
-        
+
         spawn_obstacle(
             Vec3::X * x_pos + Vec3::Y * (get_centered_pipe_position() + y_offset),
             1.,
@@ -160,16 +211,13 @@ fn spawn_obstacles(
     }
 }
 
-
 fn generate_offset(rand: &mut ThreadRng) -> f32 {
     rand.random_range(-OBSTACLE_VERTICAL_OFFSET..OBSTACLE_VERTICAL_OFFSET) * PIXEL_RATIO
 }
 
-
 fn get_centered_pipe_position() -> f32 {
     (OBSTACLE_HEIGHT / 2. + OBSTACLE_GAP_SIZE) * PIXEL_RATIO
 }
-
 
 fn spawn_obstacle(
     translation: Vec3,
@@ -179,6 +227,7 @@ fn spawn_obstacle(
 ) {
     commands.spawn((
         Sprite::from_image(pipe_image.clone()),
+        // This chains some Transform methods to create a Transform instance with it translation and scale set
         Transform::from_translation(translation).with_scale(Vec3::new(
             PIXEL_RATIO,
             PIXEL_RATIO * -pipe_direction,
@@ -187,7 +236,6 @@ fn spawn_obstacle(
         Obstacle { pipe_direction },
     ));
 }
-
 
 fn update_obstacles(
     time: Res<Time>,
@@ -199,9 +247,28 @@ fn update_obstacles(
     for (obstacle, mut transform) in obstacle_query.iter_mut() {
         transform.translation.x -= time.delta_secs() * OBSTACLE_SCROLL_SPEED;
 
-        // if transform.translation.x + OBSTACLE_WIDTH * PIXEL_RATIO / 2. < game_manager.window_dimensions.x /2. {
-        //     transform.translation.x += OBSTACLE_AMOUNT as f32 * OBSTACLE_SPACING * PIXEL_RATIO;
-        //     transform.translation.y = get_centered_pipe_position() * obstacle.pipe_direction + y_offset;
-        // }
+        if (transform.translation.x + OBSTACLE_WIDTH * PIXEL_RATIO / 2.)
+            < -game_manager.window_dimensions.x / 2.
+        {
+            transform.translation.x += OBSTACLE_AMOUNT as f32 * OBSTACLE_SPACING * PIXEL_RATIO;
+            transform.translation.y =
+                get_centered_pipe_position() * obstacle.pipe_direction + y_offset;
+        }
+    }
+}
+
+fn debug_cursor_position(
+    query_window: Query<&Window>,
+    query_camera: Query<(&Camera, &GlobalTransform)>,
+) {
+    let (camera, camera_transform) = query_camera.single().unwrap();
+    let window = query_window.single().unwrap();
+
+    if let Some(world_position) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor).ok())
+        .map(|ray| ray.origin.truncate())
+    {
+        info!("Screen Position: {}", world_position);
     }
 }
